2496633eed82c9053db1a0509ebbdea795aa680c
Dmitriy Konovalov <konovalov.dmitriy.vladimirovich@gmail.com>
2017-05-12 12:06:05 +0300

    new features

---
+++ CSimplest/Documents/DcWithHtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CSimplest.Common;
+using CSimplest.Documents.Interfaces;
+
+namespace CSimplest.Documents
+{
+    public sealed class DcWithHtml : DcHtml
+    {
+        private readonly DcText _origin;
+        public DcWithHtml(DcText origin)
+        {
+            _origin = origin;
+        }
+        public Text AsHtml()
+        {
+            return _origin.AsText();
+        }
+
+        public Text AsText()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Unwrap()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Sample/Pages/MainPage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using CSimplest.Common;
+using CSimplest.Documents.Interfaces;
+using CSimplest.Documents;
+using System.Collections.Generic;
+
+namespace Sample.Pages
+{
+    public sealed class MainPage : DcHtml
+    {
+        public Text AsHtml()
+        {
+            return new DcTemplate(
+                new DcFile(
+                    new FilePath("~/App_Data/Templates/Html/MainPage.html").Unwrap()
+                ),
+                new Dictionary<string, Text>()
+                {
+                    
+                }
+            ).AsText();
+        }
+
+        public Text AsText()
+        {
+            return new PlainText(Unwrap());
+        }
+
+        public string Unwrap()
+        {
+            return nameof(MainPage);
+        }
+    }
+}
